291f1229caa7a121df7ea99a897323c6788d149b
Bertolda <boris.sen4ikov1994@yandex.ru>
2020-02-12 21:24:57 +0200

    delete button show

--- app/src/main/java/com/example/showyouripadress/MainActivity.java
+++ app/src/main/java/com/example/showyouripadress/MainActivity.java
@@ -5,23 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.text.format.Formatter;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
-    Button showBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        showBtn = findViewById(R.id.showBtn);
         textView = findViewById(R.id.getIp);
-    }
 
-    public void show(View view) {
         WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
 
         String ipAddress = Formatter.formatIpAddress(wifiManager.getConnectionInfo().getIpAddress());
@@ -29,4 +25,6 @@ public class MainActivity extends AppCompatActivity {
         textView.setText("Your Device IP Address: " + ipAddress);
 
     }
+
+
 }
